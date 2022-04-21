4972c22527dfd1accb1f88014007d3c21966efc2
Eric McGary <eric@rhove.com>
2022-04-21 12:24:54 -0400

    added a way to retrieve a record

--- whitelist/cli/cli.js
+++ whitelist/cli/cli.js
@@ -4,6 +4,7 @@ const {Command} = require("commander");
 const program = new Command();
 const anchor = require("@project-serum/anchor");
 const fs = require("fs");
+const {Program} = require("@project-serum/anchor");
 const loadKeypair = require("./utils/load-keypair").loadKeypair;
 
 const PROGRAM_ID = new anchor.web3.PublicKey("JDS6WitBF654whkcWz5i5HX1ixska8Je4ahw7XYS7h5A");
@@ -22,9 +23,14 @@ const GROUP_PUBKEY = new anchor.web3.PublicKey("87ZjYTenZmy9pjXnYH79mkX1tEKnkcY9
             "-u, --user <string>",
             "Keypair file location of user who is going to have a record about.",
         )
+        .requiredOption(
+            "-a, --authority <string>",
+            "Keypair file location of account that will have authority over the whitelist account.",
+        )
         .action(async (options) => {
 
             let userKeypair = await loadKeypair(options.user);
+            let authority = await loadKeypair(options.authority);
 
             try {
 
@@ -48,12 +54,54 @@ const GROUP_PUBKEY = new anchor.web3.PublicKey("87ZjYTenZmy9pjXnYH79mkX1tEKnkcY9
                         signer: userKeypair.publicKey,
                         record: account,
                         systemProgram: anchor.web3.SystemProgram.programId,
+                        authority: authority.publicKey
                     },
                     signers: [userKeypair],
                 });
 
-
                 console.log(tx);
+
+            } catch (error) {
+
+                console.error(error);
+
+            }
+
+        });
+
+    program.command("get-record")
+        .description("Gets the information assoaciated with a record.")
+        .requiredOption(
+            "-u, --user <string>",
+            "Keypair file location of user who the record is about.",
+        )
+        .action(async (options) => {
+
+            let userKeypair = await loadKeypair(options.user);
+
+            try {
+
+                let config = fs.readFileSync(process.env.CONFIG);
+                config = String(config);
+
+                let rpcUrl = config.match(".*(?:json_rpc_url: )(.*)")[1]
+                process.env.ANCHOR_PROVIDER_URL = rpcUrl.replace(/(")+/gi, "")
+
+                anchor.setProvider(anchor.AnchorProvider.env());
+
+                const [account, bump] = await anchor.web3.PublicKey.findProgramAddress([
+                    GROUP_PUBKEY.toBytes(),
+                    userKeypair.publicKey.toBytes()
+                ], PROGRAM_ID);
+
+                const whitelist = anchor.workspace.Whitelist;
+
+                const meta = await whitelist.account.metadata.fetch(account);
+
+                console.log("Meta: ");
+                console.log(meta);
+
+                process.exit(1);
 
             } catch (error) {
 
