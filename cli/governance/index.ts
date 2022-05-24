b9226ab8ce3b9c36790ce5472ec50196eaaf3515
Eric McGary <eric@rhove.com>
2022-05-23 20:42:49 -0400

    update gitignore

--- cli/governance/index.ts
+++ cli/governance/index.ts
@@ -30,18 +30,17 @@ export default (async (program: Command) => {
 
     const depositCapitalConfig = `
     {
-        "cluster": "http://localhost:8899",
-        "owner": "~/.config/solana/id.json",
-        "delegate": "~/solana-keys/peacemaker.json",
-        "governanceProgramId": "5Hyx5g6n82uZpVYRLZqssLSj5V6mZc2QYQFtPcj83Jp2",
-        "identityVerificationProgramId": "3YC2irJKAzmuqeg2Qf9v8YBb1ufGmYTuvggxqv4bCyST",
-        "usdcMint": "Ha46W7m15Pviwwv95AnjXThFxum3nhcjEdvSw9GxxTWA",
-        "realm": "98VUKFoD7KqYfBNnvyJiuKmb8sj5ws7HotZopSngxsqY",
-        "lpGovernance": "HApH8Zr8fqXyoH3xyjaj4PBdgWyCDPiGevu4ijTCGb9E",
-        "lpMint": "HzhBrBLD8NQrE1jwHNptvmFmgQ1nddtHTamWy7eqLtTJ",
-        "delegateMintGovernance": "UW21kChKwbPjtrYipcGqWXrKkG3XvHchKYgbLdauhCB",
-        "delegateTokenMint": "F2RdujrSfffnTc1Qps8bEtxW25Wm2j1u7d2Ph3ZLQKzt",
-        "realm": "98VUKFoD7KqYfBNnvyJiuKmb8sj5ws7HotZopSngxsqY"
+      "cluster": "http://localhost:8899",
+      "owner": "~/.config/solana/id.json",
+      "governanceProgramId": "5Hyx5g6n82uZpVYRLZqssLSj5V6mZc2QYQFtPcj83Jp2",
+      "identityVerificationProgramId": "3YC2irJKAzmuqeg2Qf9v8YBb1ufGmYTuvggxqv4bCyST",
+      "usdcMint": "Ha46W7m15Pviwwv95AnjXThFxum3nhcjEdvSw9GxxTWA",
+      "realm": "98VUKFoD7KqYfBNnvyJiuKmb8sj5ws7HotZopSngxsqY",
+      "lpGovernance": "HApH8Zr8fqXyoH3xyjaj4PBdgWyCDPiGevu4ijTCGb9E",
+      "lpMint": "HzhBrBLD8NQrE1jwHNptvmFmgQ1nddtHTamWy7eqLtTJ",
+      "delegateMintGovernance": "UW21kChKwbPjtrYipcGqWXrKkG3XvHchKYgbLdauhCB",
+      "delegateTokenMint": "F2RdujrSfffnTc1Qps8bEtxW25Wm2j1u7d2Ph3ZLQKzt",
+      "amount": 1
     }
     `
 
@@ -64,6 +63,7 @@ export default (async (program: Command) => {
         .addHelpText("afterAll", `\nExample Config File:\n${daoConfig}`);
 
     command.command("deposit-capital")
+        .alias("deposit")
         .description("Deposit capital into an investment DAO.")
         .requiredOption(
             "-i, --input-file <string>",
