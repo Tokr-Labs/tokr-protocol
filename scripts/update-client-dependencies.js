953d6cb3d04ee8780b5fa31dd76df1d998f8b8ca
Eric McGary <eric@rhove.com>
2022-05-25 14:57:49 -0400

    initial update script complete

--- scripts/update-client-dependencies.js
+++ scripts/update-client-dependencies.js
@@ -1,24 +1,54 @@
-// reference all package.json files
-// find any reference to anchor, spl-token or web3 and set their values to match
-// "@project-serum/anchor": "0.24.2",
-// "@solana/spl-token": "0.2.0",
-// "@solana/web3.js": "1.43.1",
-
 const replace = require("replace-in-file");
-const options = {
-    files: [
+
+(async () => {
+
+    const ANCHOR_VERSION = "0.24.2";
+    const SPL_TOKEN_VERSION = "0.24.2";
+    const WEB3_VERSION = "1.43.1";
+
+    const files = [
         "./package.json",
         "./cli/package.json",
         "./programs/**/client/package.json"
-    ],
-    from: /"@project-serum\/anchor":\s+".+"/g,
-    to: '"@project-serum/anchor": "0.24.2"'
-};
+    ]
 
-try {
-    const results = await replace(options)
-    console.log('Replacement results:', results);
-}
-catch (error) {
-    console.error('Error occurred:', error);
-}
+    const anchorDependencyOptions = {
+        files: files,
+        from: /"@project-serum\/anchor":\s+".+"/g,
+        to: `"@project-serum/anchor": "${ANCHOR_VERSION}"`
+    };
+
+    const splTokenOptions = {
+        files: files,
+        from: /"@solana\/spl-token":\s+".+"/g,
+        to: `"@solana/spl-token": "${SPL_TOKEN_VERSION}"`
+    };
+
+    const web3Options = {
+        files: files,
+        from: /"@solana\/web3\.js":\s+".+"/g,
+        to: `"@solana/web3.js": "${WEB3_VERSION}"`
+    };
+
+    try {
+
+        let anchorReplaceResults = await replace(anchorDependencyOptions)
+        let splTokenResults = await replace(splTokenOptions)
+        let web3Results = await replace(web3Options)
+
+        console.log("anchorReplaceResults:")
+        console.log(anchorReplaceResults)
+
+        console.log("splTokenResults:")
+        console.log(splTokenResults)
+
+        console.log("web3Results:")
+        console.log(web3Results)
+
+    } catch (error) {
+
+        console.error('Error occurred:', error);
+
+    }
+
+})();
