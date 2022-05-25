d77ad9ae17b40a73315b8483350131d939a2918f
Eric McGary <eric@rhove.com>
2022-05-24 23:12:29 -0400

    scaffold script to update node dependencies

---
+++ scripts/update-client-dependencies.js
@@ -0,0 +1,5 @@
+// reference all package.json files
+// find any reference to anchor, spl-token or web3 and set their values to match
+// "@project-serum/anchor": "0.24.2",
+// "@solana/spl-token": "0.2.0",
+// "@solana/web3.js": "1.43.1",
