54e5e1d262a407574d4a550435b6e0a397443302
Eric McGary <eric@rhove.com>
2022-05-25 14:47:09 -0400

    wip

--- scripts/update-client-dependencies.js
+++ scripts/update-client-dependencies.js
@@ -3,3 +3,22 @@
 // "@project-serum/anchor": "0.24.2",
 // "@solana/spl-token": "0.2.0",
 // "@solana/web3.js": "1.43.1",
+
+const replace = require("replace-in-file");
+const options = {
+    files: [
+        "./package.json",
+        "./cli/package.json",
+        "./programs/**/client/package.json"
+    ],
+    from: /"@project-serum\/anchor":\s+".+"/g,
+    to: '"@project-serum/anchor": "0.24.2"'
+};
+
+try {
+    const results = await replace(options)
+    console.log('Replacement results:', results);
+}
+catch (error) {
+    console.error('Error occurred:', error);
+}
